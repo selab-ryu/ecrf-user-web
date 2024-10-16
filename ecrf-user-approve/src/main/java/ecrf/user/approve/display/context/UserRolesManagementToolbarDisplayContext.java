87a2ca717124d6dfc72c22cbd4260d94b167177c
selab-ryu <hkjkkl@yonsei.ac.kr>
2024-10-16 15:15:45 +0900

    [Site Role]
    - Update Site Role
      - add MVC Command
      - add functionality
    
    - Delete Membership
      - add MVC Coomand
      - add functionality

---
+++ ecrf-user-approve/src/main/java/ecrf/user/approve/display/context/UserRolesManagementToolbarDisplayContext.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package ecrf.user.approve.display.context;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.display.context.SearchContainerManagementToolbarDisplayContext;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+
+import javax.portlet.PortletURL;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author SELab-Ryu
+ *
+ */
+public class UserRolesManagementToolbarDisplayContext extends SearchContainerManagementToolbarDisplayContext {
+
+	public UserRolesManagementToolbarDisplayContext(
+			LiferayPortletRequest liferayPortletRequest,
+			LiferayPortletResponse liferayPortletResponse,
+			HttpServletRequest httpServletRequest,
+			UserRolesDisplayContext userRolesDisplayContext)
+		throws PortalException {
+
+		super(
+			liferayPortletRequest, liferayPortletResponse, httpServletRequest,
+			userRolesDisplayContext.getRoleSearchSearchContainer());
+	}
+
+	@Override
+	public String getClearResultsURL() {
+		PortletURL clearResultsURL = getPortletURL();
+
+		clearResultsURL.setParameter("keywords", StringPool.BLANK);
+
+		return clearResultsURL.toString();
+	}
+
+	@Override
+	public String getComponentId() {
+		return "userGroupRoleRoleManagementToolbar";
+	}
+
+	@Override
+	public String getSearchActionURL() {
+		PortletURL searchActionURL = getPortletURL();
+
+		return searchActionURL.toString();
+	}
+
+	@Override
+	public String getSearchContainerId() {
+		return "userGroupRoleRole";
+	}
+
+	@Override
+	protected String[] getDisplayViews() {
+		return new String[] {"list", "descriptive", "icon"};
+	}
+
+	@Override
+	protected String[] getNavigationKeys() {
+		return new String[] {"all"};
+	}
+
+	@Override
+	protected String[] getOrderByKeys() {
+		return new String[] {"title"};
+	}
+}
