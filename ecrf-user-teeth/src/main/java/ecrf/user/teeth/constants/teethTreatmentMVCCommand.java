8a5cb7ce24e0ca8bb8668b680592a366f73d3d77
dev-ryu <dev-ryu@dhsoft.co.kr>
2025-10-21 09:48:37 +0900

    [Teeth]
    - delete ecrf-user-teeth module
      - teeth module is integrated into crf module

--- ecrf-user-teeth/src/main/java/ecrf/user/teeth/constants/teethTreatmentMVCCommand.java
+++
@@ -1,7 +0,0 @@
-package ecrf.user.teeth.constants;
-
-public class teethTreatmentMVCCommand {
-	public static final String ADD_TREATMENT = "/teeth/addTreatment";
-	public static final String EDIT_TREATMENT = "/teeth/editTreatment";
-	public static final String DELETE_TREATMENT = "/teeth/deleteTreatment";
-}
